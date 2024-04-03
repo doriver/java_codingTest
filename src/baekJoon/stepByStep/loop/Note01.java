7de4dbc026eda8adce75286a68b095af35d4f2ce
doriver <tkdduq241@naver.com>
2024-04-03 20:33:11 +0900

    sum += a * b 이런 형태도 가능

--- src/baekJoon/stepByStep/loop/Note01.java
+++ src/baekJoon/stepByStep/loop/Note01.java
@@ -7,6 +7,8 @@ public class Note01 {
  		for (int i = 1; i <= 10; i++) {
  			sum += i; // i를 다 더한값 ( 앞변수에, 뒤변수를 더해주는거 )
  		}
+// 		sum += i * (i+1) ; 이런 형태도 생각 가능
+ 		
  		System.out.println(sum); // 55
 	}
 
