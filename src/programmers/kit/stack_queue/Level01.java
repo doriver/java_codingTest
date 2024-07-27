2f84ae4e6f0409d39368426b70e2d4c438a70a3f
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-07-27 16:26:41 +0900

    stack으로 연속된 두수 중복 안되게

--- src/programmers/kit/stack_queue/Level01.java
+++ src/programmers/kit/stack_queue/Level01.java
@@ -1,5 +1,35 @@
 package programmers.kit.stack_queue;
 
-public class Level01 {
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 
+public class Level01 {
+/*
+ * 연속된 두 수를 비교위해
+ * stack에 넣으면서
+ * 맨 마지막에 넣은거랑, 앞으로 넣을거 비교
+ * 
+ * 맨 마지막에 넣은거를 peek할수 있어야함
+ */
+	
+	public int[] solution(int []arr) {
+        int[] answer = {};
+        
+        Stack<Integer> st = new Stack<>();
+    	st.push(arr[0]);
+    	
+    	for (int i = 1; i < arr.length; i++) {
+    		if (st.peek() != arr[i]) {
+    			st.push(arr[i]);
+    		}
+    	}
+
+    	answer = new int[st.size()];
+    	
+    	for (int i = 0; i < answer.length; i++) {
+    		answer[answer.length - i - 1] = st.pop();
+    	}
+        return answer;
+    }
 }
