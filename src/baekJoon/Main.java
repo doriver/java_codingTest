691906524af7dd9824a571dfdb359b0e3e0c97f7
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-08-28 20:20:54 +0900

    코테 문자열 기초 품

--- src/baekJoon/Main.java
+++ src/baekJoon/Main.java
@@ -12,7 +12,8 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 		int N = Integer.parseInt(br.readLine());
+		String str = br.readLine();
+		int N = Integer.parseInt(br.readLine());
  		
  		br.close();
 
--- src/baekJoon/stepByStep/string/Note01.java
+++ src/baekJoon/stepByStep/string/Note01.java
@@ -1,5 +1,29 @@
 package baekJoon.stepByStep.string;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Note01 {
 
+	public static void main(String[] args) throws IOException {
+
+		
+/*
+ * 문자열의 i번째 글자 뽑기
+ * char 문자열변수 = 문자열.charAt(12) // 12번째 문자1개만 반환		
+ */
+		String a = "asdklfjl";
+		String b = "강낭콩";
+		
+		System.out.println(a.charAt(2)); // d
+		System.out.println(b.charAt(2)); // 콩
+/*
+ * 문자열의 길이
+ * 문자열.length()		
+ */
+		System.out.println(a.length()); // 8
+		System.out.println(b.length()); // 3
+	}
 }
