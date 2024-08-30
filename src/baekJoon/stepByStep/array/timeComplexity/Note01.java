72a542bb51f934f7b3dc042b4cb05ad5ee30c1ca
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-08-30 18:52:25 +0900

    시간복잡도 시작

---
+++ src/baekJoon/stepByStep/array/timeComplexity/Note01.java
@@ -0,0 +1,11 @@
+package baekJoon.stepByStep.array.timeComplexity;
+
+public class Note01 {
+
+	// n에따라, 특정 코드의 수행횟수 > 특정 코드의 수행횟수를 다항식으로 나태냈을때 최고차항의 차수
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
