a61fa23e377a71de866dcf8ac0e10f46f8785561
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-09-03 16:43:23 +0900

    시간복잡도 2문제 품

--- src/baekJoon/stepByStep/timeComplexity/Note01.java
+++ src/baekJoon/stepByStep/timeComplexity/Note01.java
@@ -5,7 +5,13 @@ public class Note01 {
 	// n에따라, 특정 코드의 수행횟수 > 특정 코드의 수행횟수를 다항식으로 나태냈을때 최고차항의 차수
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		for (int i=0; i< 6; i++) {
+			for (int j=0; j<7; j++) {
+				// for문은 횟수측면에서, 시그마(더하는거)임 7 + 7 + 7 + 7 + 7 + 7 
+					// n이면 n*n
+			}	
+		}
 	}
 
 }
