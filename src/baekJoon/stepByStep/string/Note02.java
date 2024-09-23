151259ecd78dc9d734fa2ed021d4b53a0ff29ef6
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2024-09-23 13:55:34 +0900

    문제 품

--- src/baekJoon/stepByStep/string/Note02.java
+++ src/baekJoon/stepByStep/string/Note02.java
@@ -19,13 +19,20 @@ public class Note02 {
 		System.out.println(sb); // 만든 별 찍힘
 		
 //	StringTokenizer( "문자열" , 구분자 );
-		StringTokenizer st = new StringTokenizer("가 나다 라 바사"," "); 
+		StringTokenizer st = new StringTokenizer("가 나다 라 바사  "," "); 
+		System.out.println(st.countTokens()); // 4
 		System.out.println(st.nextToken()); // 가
 		System.out.println(st.nextToken()); // 나다
 		System.out.println(st.nextToken()); // 라
 		System.out.println(st.nextToken()); // 바사
 
-
+		System.out.println(st.countTokens()); // 0
+		
+//	String > int , long
+		String a = "1234";
+		int ai = Integer.parseInt(a);
+		long al = Long.parseLong(a);
+		System.out.println(ai + " " + al); // 1234 1234
 		
 	}
 
